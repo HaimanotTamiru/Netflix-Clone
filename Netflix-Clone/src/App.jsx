6ea7e06938bdd6162c6e9b4d96070a15b9092d85
HaimanotTamiru <haimikidus@gmail.com>
2024-09-03 19:33:00 -0500

    header and footer

--- Netflix-Clone/src/App.jsx
+++ Netflix-Clone/src/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
-
 import './App.css'
-
+import Row from './Components/Row/Row'
+import requests from './Components/APIKey/Requests'
+import Home from './pages/Home/Home'
 function App() {
 
 
   return (
-    <>
-     <h1>Hello World</h1>
-    </>
+    <div className="App">
+      {/* <Row title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals} />
+      <Row title="Trending Now"
+      fetchUrl={requests.fetchTrending}/> */}
+      <Home/>
+    </div>
   )
 }
 
