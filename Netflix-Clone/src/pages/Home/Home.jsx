8ccb4b556bd6b42ad14c5d13163263bcfadbdf74
HaimanotTamiru <haimikidus@gmail.com>
2024-09-04 19:39:06 -0500

    row

--- Netflix-Clone/src/pages/Home/Home.jsx
+++ Netflix-Clone/src/pages/Home/Home.jsx
@@ -3,12 +3,14 @@ import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
 // import NetflixBanner from '../../Components/Banner/NetflixBanner'
 import Banner from '../../Components/Banner/Banner'
+import RowList from '../../Components/Rows/RowList/RowList'
 function Home() {
   return (
       <>
       <Header />
       <Banner />
       {/* <NetflixBanner/> */}
+      <RowList/>
       <Footer />
       </>
   )
